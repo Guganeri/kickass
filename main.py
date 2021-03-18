8c95d6323f2c263c9eeb1c48fa1137dfe7c0af9d
Guga Neri <guganeriss@gmail.com>
2021-03-18 10:55:27 -0700

    chrome headless and loop initial

--- main.py
+++ main.py
@@ -4,16 +4,36 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-#path chrome driver browser
-driver = webdriver.Chrome(executable_path=r"")
 
-#navigate to url
-driver.get("https://extremereportbot.com/report/")
-#actions
-driver.find_element_by_name("extremereportbot").send_keys("" + Keys.RETURN)
-driver.find_element(By.ID, "IL_SR_X1").click()
-driver.implicitly_wait(10)
-driver.find_element(By.ID, "user_confirm").click()
-driver.find_element(By.CSS_SELECTOR, ".csgo > .option_label").click()
-driver.find_element(By.CSS_SELECTOR, ".ui:nth-child(3) > label").click()
-driver.find_element(By.ID, "report_confirm").click()
+PATH_WEBDRIVER="/home/android/Downloads/chromedriver"
+WEB_REPORT=""
+WINDOW_SIZE = "1920,1080"
+
+
+class Report:
+    i = 1
+    while i < 11:
+        print (f'Report: {i} | Meliante: {WEB_REPORT}')
+        i += 1
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(PATH_WEBDRIVER, options=chrome_options)
+        #driver = webdriver.Chrome(executable_path=PATH_WEBDRIVER, chrome_options=chrome_options)
+
+        #navigate to url
+        driver.get("https://extremereportbot.com/report/")
+        #actions
+        driver.find_element_by_name("extremereportbot").send_keys(WEB_REPORT + Keys.RETURN)
+        driver.find_element(By.ID, "IL_SR_X1").click()
+        driver.implicitly_wait(10)
+        driver.find_element(By.ID, "user_confirm").click()
+        driver.find_element(By.CSS_SELECTOR, ".csgo > .option_label").click()
+        driver.find_element(By.CSS_SELECTOR, ".ui:nth-child(3) > label").click()
+        driver.find_element(By.ID, "report_confirm").click()
+          
+
+#def main():
+#    #main()
+#    i = 1
+#    print('teste')
